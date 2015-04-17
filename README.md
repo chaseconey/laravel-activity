57d09101b87df179207d04de112b3dda70531d2c
Chase Coney <chasiepoo@gmail.com>
2015-04-17 08:49:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Laravel Activity Logger
+
+A very simple activity logger build specifically for laravel that tracks actions performed by users based on
+model events.
---
+++ src/ActivityProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Chaseconey\ActivityRecorder;
+
+use Illuminate\Support\ServiceProvider;
+
+class ActivityProvider extends ServiceProvider {
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+
+		$this->publishes([
+			__DIR__ . '/../database/migrations/' => database_path('/migrations')
+		], 'migrations');
+	}
+
+}
